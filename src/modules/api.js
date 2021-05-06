269b49b755d9c3584d8e7db7bfa6c64890894339
Joshua Petrovani Miller <joshpetrovanimiller@gmail.com>
2021-05-06 00:12:17 -0400

    Remove extra slash in baseurl

--- src/modules/api.js
+++ src/modules/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const base_url = 'https://ipbf5lnnbl.execute-api.us-east-1.amazonaws.com/dev/';
+const base_url = 'https://ipbf5lnnbl.execute-api.us-east-1.amazonaws.com/dev';
 
 export async function postMeeting(meetingUuid, participantDetails) {
   try {
