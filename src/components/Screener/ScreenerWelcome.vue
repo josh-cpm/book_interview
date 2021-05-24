4b57ee0b5a2b82489f960ad91bb33e1d72ae9c6e
Joshua Petrovani Miller <joshpetrovanimiller@gmail.com>
2021-05-23 22:23:57 -0400

    Add text and components to ScreenerWelcome

--- src/components/Screener/ScreenerWelcome.vue
+++ src/components/Screener/ScreenerWelcome.vue
@@ -1,3 +1,28 @@
 <template>
-  Screener Welcome
+  <div>
+    <h1>Let's Meet!</h1>
+    <p>Get paid ${{ incentive }} for sharing your thoughts with our team.</p>
+    <MeetingDetails></MeetingDetails>
+    <p>Answer a few questions to see if you qualify for this interview.</p>
+    <PrimaryCta buttonText="Lets Begin"></PrimaryCta>
+  </div>
 </template>
+
+<script>
+import MeetingDetails from '../Schedule/MeetingDetails';
+import PrimaryCta from '../Reusible/PrimaryCta';
+import store from '@/modules/store.js';
+
+export default {
+  name: 'ScreenerWelcome',
+  components: {
+    MeetingDetails,
+    PrimaryCta,
+  },
+  computed: {
+    incentive() {
+      return store.meetingDetails.incentive;
+    },
+  },
+};
+</script>
