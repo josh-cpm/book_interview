afbc517dfe4739414eac90bf33cedf1b7641b2e2
Joshua Petrovani Miller <joshpetrovanimiller@gmail.com>
2021-05-23 20:47:48 -0400

    Add color and text to footer component

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,3 +1,34 @@
 <template>
-  Footer
+  <div class="footer-container">
+    <div class="footer-content">
+      <p class="footer-content__title">
+        Interview Hero: Connect with your customers
+      </p>
+    </div>
+  </div>
 </template>
+
+<script>
+export default {
+  name: 'Footer',
+};
+</script>
+
+<style scoped>
+.footer-container {
+  margin-top: 4rem;
+  background-color: var(--color-gray-bg);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: 12rem;
+}
+
+.footer-content {
+  margin: var(--margin-sides);
+}
+
+.footer-content__title {
+  color: var(--color-primary);
+}
+</style>
