86891fed9ecf1f1b8f5042a924a6a67e854cf81b
Joshua Petrovani Miller <joshpetrovanimiller@gmail.com>
2021-06-16 21:34:17 -0400

    Fix naming issue in QuizSelectDoubleConfirm component

--- src/components/Reusible/quizSelectButons/QuizSelectDoubleConfirm.vue
+++ src/components/Reusible/quizSelectButons/QuizSelectDoubleConfirm.vue
@@ -24,7 +24,7 @@
 
 <script>
 export default {
-  name: 'QuizSelectDefault',
+  name: 'QuizSelectDoubleConfirm',
   props: ['buttonValue', 'confirmButtonValue', 'buttonData'],
   emits: ['confirm'],
   data() {
